49899c5ef975aac528242b44928680addba34904
Neri J. Jakubowski Jr <neri@nerijunior.com>
2020-01-15 19:01:03 -0300

    db for production

--- db.php
+++ db.php
@@ -8,11 +8,21 @@ require_once "Classes/Musica.php";
 require_once "Classes/Playlist.php";
 
 // conexão com o banco de dados
-$db = mysqli_connect("localhost", "root", "5544", "playlist");
+$url = getenv("DATABASE_URL");
+
+// Produção
+if ($url) {
+	$config = parse_url($url);
+	$dbName = substr($config['path'], 1);
+
+	$db = mysqli_connect($config['host'], $config['user'], $config['pass'], $dbName);
+} else {
+	$db = mysqli_connect("localhost", "root", "5544", "playlist");
+}
 
 // verificando conexão com o banco de dados
 if (!$db) {
-    echo "não conectado ao mysql " . PHP_EOL;
-    echo "debug do erro " . mysqli_connect_errno() . PHP_EOL;
-    echo "debug do erro " . mysqli_connect_error() . PHP_EOL;
+	error_log("não conectado ao mysql " . PHP_EOL);
+	error_log(mysqli_connect_errno() . PHP_EOL . mysqli_connect_error() . PHP_EOL);
+	exit("Falha na conexão com o banco de dados.");
 }
