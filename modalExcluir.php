e80c7e66391796f5258efef40135d1f8dc5646c5
GuiRufino <guilhermeliima74@gmail.com>
2020-01-23 05:59:58 -0300

    botão d0 sim, do excluir musica

--- modalExcluir.php
+++ modalExcluir.php
@@ -1,17 +1,18 @@
- <div>
-  <div class="modal fade" id="modalExemplo" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered"  role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Tem certeza que deseja excluir?</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-danger" data-dismiss="modal">não</button>
-          <button type="button" class="btn btn-success"><a style="text-decoration:none; color: white;" href="excluir.php?musicaId=<?php echo $row['id']; ?>">sim</a></button>
-        </div>
+<div class="modal fade" id="modalExemplo" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Excluir música</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <h5>Deseja excluir a música <i><b><?php echo $row['nomeMusica']; ?></b></i>?</h5>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">não</button>
+        <button type="button" class="btn btn-success"><a style="text-decoration:none; color: white;" href="excluir.php?musicaId=<?php echo $row['id']; ?>">sim</a></button>
       </div>
     </div>
   </div>
