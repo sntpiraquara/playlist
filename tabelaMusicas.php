b762769760ee239a0da4573ff6672c76cb2a7e1b
GuiRufino <guilhermeliima74@gmail.com>
2020-01-23 00:04:48 -0300

    ajuste modal

--- tabelaMusicas.php
+++ tabelaMusicas.php
@@ -32,22 +32,24 @@ if (mysqli_num_rows($todasMusicas) > 0) {
                 <td><?php echo $row['nomeMusica']; ?></td>
                 <td><?php echo $row['nomeArtista']; ?></td>
                 <td><?php echo $row['tipo']; ?></td>
-                <td><button type= "button" class= "btn btn-primary btn-musica-editar">Editar</button></td>
+                <td><button type="button" class= "btn btn-primary btn-musica-editar">Editar</button></td>
                 <td><button type="button" class="btn btn-danger" data-toggle="modal" data-target="#modalExcluir">
                  excluir
                </button></td>
-               <div class="modal fade" id="modalExcluir" tabindex="-1" role="dialog" aria-labelledby="excluir" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered container"  role="document">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h5 class="modal-title" id="excluir">Tem certeza que deseja excluir?</h5>
-                      <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
-                        <span aria-hidden="true">&times;</span>
-                      </button>
-                    </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-danger" data-dismiss="modal">não</button>
-                      <a type="button" class="btn btn-success" href="excluir.php?musicaId=<?php echo $row['id']; ?>">sim</a>
+               <div class="container">
+                 <div class="modal fade" id="modalExcluir" tabindex="-1" role="dialog" aria-labelledby="excluir" aria-hidden="true">
+                  <div class="modal-dialog modal-dialog-centered"  role="document">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="excluir">Tem certeza que deseja excluir?</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">não</button>
+                        <a type="button" class="btn btn-success" href="excluir.php?musicaId=<?php echo $row['id']; ?>">sim</a>
+                      </div>
                     </div>
                   </div>
                 </div>
