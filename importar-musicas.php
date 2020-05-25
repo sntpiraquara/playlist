0be349b78fc544711d39eb28f3aba509ac9528c7
Neri J. Jakubowski Jr <neri@nerijunior.com>
2020-05-25 01:26:22 -0300

    importar-musicas

---
+++ importar-musicas.php
@@ -0,0 +1,69 @@
+<?php
+
+require "config.php";
+
+error_reporting(E_ERROR | E_PARSE);
+
+function htmlToDOMDocument($url)
+{
+    $html = file_get_contents($url);
+    $doc = new DOMDocument();
+    $doc->loadHTML(mb_convert_encoding($html, 'HTML-ENTITIES', 'UTF-8'));
+    return $doc;
+}
+
+function getListaArtistas()
+{
+    $urlArtistasMaisOuvidos = 'https://www.letras.mus.br/mais-acessadas/gospelreligioso/';
+
+    $doc = htmlToDOMDocument($urlArtistasMaisOuvidos);
+
+    $XPath = new DOMXPath($doc);
+    $links = $XPath->query('//ol[contains(@class, "top-list_art")]/li/a');
+
+    $artistas = [];
+
+    foreach ($links as $link) {
+        $artistas[] = [
+            'nome' => trim($link->textContent),
+            'url'  => $link->getAttribute('href'),
+        ];
+    }
+
+    sort($artistas);
+
+    return $artistas;
+}
+
+function getMusicasDoArtista($artista)
+{
+    $listaMusicasElemento = '.cnt-list--alp .cnt-list-row.-song a';
+
+    $doc = htmlToDOMDocument('https://www.letras.mus.br' . $artista['url']);
+    $XPath = new DOMXPath($doc);
+    $links = $XPath->query('//a[contains(@class, "song-name")]');
+
+    $musicas = [];
+
+    foreach ($links as $link) {
+        $musicas[] = [
+            'titulo' => trim($link->textContent),
+            'url'    => $link->getAttribute('href'),
+        ];
+    }
+
+    return $musicas;
+}
+
+$artistas = getListaArtistas();
+
+foreach ($artistas as $artista) {
+    $musicas = getMusicasDoArtista($artista);
+
+    foreach ($musicas as $musica) {
+		$model = new Musica($db);
+		$model->artista = $artista['nome'];
+		$model->nome = $musica['titulo'];
+		$model->cadastrar();
+    }
+}
