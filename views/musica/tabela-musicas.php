240038fda234eca4899a24ecf86a9a5b6ca28623
GuiRufino <guilhermeliima74@gmail.com>
2020-05-27 20:16:04 -0300

    importação de musica do site letras-Br

--- views/musica/tabela-musicas.php
+++ views/musica/tabela-musicas.php
@@ -5,29 +5,25 @@ $total_registros_por_pagina = "10";
 $inicio = $pc - 0;
 $inicio = $inicio * $total_registros_por_pagina;
 
-$query = "SELECT
-m.id,
-m.nomeMusica,
-m.id_artista,
-a.nome
-FROM musicas m
-INNER JOIN artistas a ON m.id_artista = a.id LIMIT " . $inicio;
+$query_todos = "SELECT m.id, m.nomeMusica, m.id_artista, a.nome
+FROM musicas m INNER JOIN artistas a
+ON m.id_artista = a.id";
 
-// $limite = mysqli_query($db, $query);
-$todos = mysqli_query($db, $query);
+$query_limite = $query_todos . " LIMIT " . $inicio;
+$limite = mysqli_query($db, $query_limite);
+$todos = mysqli_query($db, $query_todos);
 
 $count_rows = mysqli_num_rows($todos); // verifica o numero total de registos
 
 $count_pages = $count_rows / $total_registros_por_pagina; // verifica a quantidade de paginas
 
 //criando os botões de anterios e proximo
-
 $anterior = $pc - 1;
 $proximo = $pc + 1;?>
 <div class="col-xl-12 col-sm-12">
 	<h3>Músicas</h3>
-	<table class="table table-striped table-bordered">
-		<thead>
+		<table class="table table-striped table-bordered">
+			<thead>
 			<tr>
 				<th>Nome</th>
 				<th>Artista</th>
@@ -43,23 +39,22 @@ $proximo = $pc + 1;?>
 					</td>
 				</tr>
 			<?php else:
-    while ($musica = mysqli_fetch_assoc($todos)): ?>
-
-																				<tr>
-																				<form action="editar.php" method="post">
-																					<input type="hidden" name="id" value="<?=$musica['id'];?>">
-																									<input type="hidden" name="nome" value="<?=$musica['nomeMusica'];?>">
-																									<input type="hidden" name="artista" value="<?=$musica['id_artista'];?>">
-																								</form>
-																								<td><?=$musica['nomeMusica'];?></td>
-																								<td><?=$musica['nome'];?></td>
-																								<td>
-																									<button type="button" class= "btn btn-primary btn-musica-editar">Editar</button>
-																									<button type="button" class="btn btn-danger btn-musica-excluir">Excluir</button>
-																								</td>
-																							</tr>
-																						<?php endwhile;?>
-					<?php endif;?>
+    while ($musica = mysqli_fetch_assoc($limite)): ?>
+							<tr>
+								<form action="editar.php" method="post">
+									<input type="hidden" name="id" value="<?=$musica['id'];?>">
+									<input type="hidden" name="nome" value="<?=$musica['nomeMusica'];?>">
+									<input type="hidden" name="artista" value="<?=$musica['id_artista'];?>">
+								</form>
+								<td><?=$musica['nomeMusica'];?></td>
+								<td><?=$musica['nome'];?></td>
+								<td>
+									<button type="button" class= "btn btn-primary btn-musica-editar">Editar</button>
+									<button type="button" class="btn btn-danger btn-musica-excluir">Excluir</button>
+								</td>
+							</tr>
+						<?php endwhile;?>
+			<?php endif;?>
 		</tbody>
 	</table>
 	<nav aria-label="Navegação de página exemplo">
@@ -76,4 +71,3 @@ if ($pc < $count_pages): ?>
 <?php
 require_once 'modals/modal-editar.php';
 require_once 'modals/modal-excluir.php';
-?>
